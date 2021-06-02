3939fe02a89e6d32ff2633ca915ea9026854f49e
王佳祥 <18834233639@163.com>
2021-06-02 00:17:12 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - Mr.Wang</div>
+                <div class="title">Hello Laravel! - 王佳祥</div>
             </div>
         </div>
     </body>
