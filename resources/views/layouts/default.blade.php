e07d331540befa455c76a1f170d01adeed5ad198
王佳祥 <18834233639@163.com>
2021-06-04 12:59:17 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     <title>@yield('title','Weibo App')</title>
 </head>
 <body>
+    <!-- 导航 -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
     <!-- 该站位区域将用于显示content区块的内容 -->
-    @yield('content')
+    <div class="container">
+      @yield('content')
+    </div>
 </body>
 </html>
