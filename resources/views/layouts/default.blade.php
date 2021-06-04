cea344d373acac6700c46e5cada13b077839d259
王佳祥 <18834233639@163.com>
2021-06-04 13:18:05 +0000

    切割头部和尾部子图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,18 +9,11 @@
 </head>
 <body>
     <!-- 导航 -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
+    @include('layouts._header')
     <!-- 该站位区域将用于显示content区块的内容 -->
     <div class="container">
       @yield('content')
     </div>
+    @include('layouts._footer')
 </body>
 </html>
